5990d0338c8cee9abc43a197eb0d5379169ae998
willhuff0 <wehuffman1@gmail.com>
2025-09-08 23:08:07 -0400

    Use find_package instead of FetchContent for common dependencies

--- common/math/include/slgl/common/Math.hpp
+++ common/math/include/slgl/common/Math.hpp
@@ -2,10 +2,9 @@
 
 #define SLGL_NAN std::numeric_limits<float>::quiet_NaN()
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/quaternion.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
-#define GLM_ENABLE_EXPERIMENTAL
-#include "glm/gtx/euler_angles.hpp"
-#include "glm/gtx/matrix_decompose.hpp"
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
